eedc68742ec4322ef0f14cbe0bf0c052832948f0
aadyag676 <aadyag676@gmail.com>
2025-09-29 10:14:50 +0530

    about.py

--- pages/about.py
+++ pages/about.py
@@ -1,9 +1,9 @@
 import streamlit as st
 st.title("about the dataset")
-st.subheader("This dataset captures 25 major cybersecurity incidents and data breaches reported in 2024 across multiple industries and countries"
-Each record provides detailed insights into the attack type, entry vector, compromised data, financial losses, recovery times, regulatory fines, and stock/customer impacts")
-
-  ("In addition to incident details, 
-the dataset includes information about the affected organizations security posture—such as patch status,
- MFA adoption, security training, CISO presence, and budget allocation—making it valuable for analyzing risk factors,
- trends, and correlations in cybersecurity")
+st.subheader("This dataset captures 25 major cybersecurity incidents and data breaches reported in 2024 across multiple" \
+"industries and countries. each record provides obtained insights into the attack type, entry vector,compromised data,financial losses," \
+"recovery times,regulatory fines,and stock customer impacts,\
+In addition to incident details, the dataset includes information about the affected organizations security postures such as,"
+"patch status ,MFA adoption, security training, CISO precence , and budget allocation - making it valuable for analyzing risk factors,"
+"trends and correlatins in cybersecurity."
+)
